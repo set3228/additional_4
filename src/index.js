d1e7a248341885c7421c891765121d2f34c485d9
set3228 <2012ynwa@gmail.com>
2017-09-27 20:47:11 +0300

    solution was added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+	var multi = new Array(200).fill(0);
+
+	for(var i = first.length - 1; i >= 0; i--) {
+		var carry = 0;
+		for(var j = second.length - 1; j >= 0; j--) {
+			var product = +first[i] * +second[j];
+			/* Определяем позицию вставки результата*/
+			var answerPosition = (multi.length - 1) - (first.length - 1 - i) - (second.length - 1 - j);
+
+			product += multi[answerPosition] + carry;
+
+			multi[answerPosition] = product % 10;
+
+			carry = Math.floor(product / 10);
+		}
+
+		multi[answerPosition - 1] = carry;
+
+		carry = 0;
+   }
+   multi = multi.join('').replace(/^0+/, '')
+	
+	return multi;
+ }
